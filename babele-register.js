25390c92bea0f0584593e752d66405d8ea005ba5
zeteticl <cougar.l@hotmail.com>
2023-02-13 17:50:35 +0800

    Update babele-register.js

--- babele-register.js
+++ babele-register.js
@@ -2,9 +2,9 @@
 Hooks.once('init', () => {
 
 	if(typeof Babele !== 'undefined') {
-		
+		console.log('Babele is active. Registering module translations')
 		Babele.get().register({
-			module: 'pokerole-babele-zh-tw',
+			module: 'pokerole-babele-zh',
 			lang: 'zh-tw',
 			dir: 'compendium'
 		});
